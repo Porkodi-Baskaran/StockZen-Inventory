51f8d0312f850968fedbdd0681797ee6bd5d1ea7
Porkodi <porkodibaskarn27@gmail.com>
2025-11-19 15:13:20 +0530

    Added new files

---
+++ inventory/src/test/java/com/stockzen/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockzen.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
